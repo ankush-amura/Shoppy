ee8caf00a95fdd861e0a50507595a3a4812cfbe4
ankush-amura <ankush@amuratech.com>
2017-09-07 10:43:07 +0530

    used Faker in Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Superadmin.create(name: Faker::Name.name , email: Faker::Internet.free_email ,
+  password: Faker::Internet.password(8) , contact: "1239873129")
+Superadmin.create(name: Faker::Name.name , email: Faker::Internet.free_email ,
+  password: Faker::Internet.password(8) , contact: "1239873128")
+Superadmin.create(name: Faker::Name.name , email: Faker::Internet.free_email ,
+  password: Faker::Internet.password(8) , contact: "1239873127")
+Superadmin.create(name: Faker::Name.name , email: Faker::Internet.free_email ,
+  password: Faker::Internet.password(8) , contact: "1239873126")
+Superadmin.create(name: Faker::Name.name , email: Faker::Internet.free_email ,
+  password: Faker::Internet.password(8) , contact: "1239873125")
